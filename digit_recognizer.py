70ba646e5c11be2bc4965665d5e18c8fd6cc2a77
perborgen <perhb@hotmail.com>
2015-05-19 13:55:29 +0100

    data

---
+++ digit_recognizer.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from sklearn import neighbors
+
+
+df = pd.read_csv("data.csv", header=0)
+
+y = df["label"]
+X = df.drop("label", axis=1)
+
+y = np.array(y)
+X = np.array(X)
+
+y_train = y[0:999]
+X_train = X[0:999]
+
+y_test = y[1000:1200]
+X_test = X[1000:1200]
+
+classifier = neighbors.KNeighborsClassifier()
+
+print 'about to fit'
+classifier.fit(X_train,y_train)
+
+
+score = classifier.score(X_test,y_test)
+
+
+reshaped_example = X[-1].reshape(28,28)
+
+image = plt.imshow(reshaped_example,cmap = cm.Greys_r)
+#plt.colorbar(image, orientation="horizontal")
+plt.show()
+
+
+print classifier.predict(X[-1])
+
+
+
+print 'score: ', score
